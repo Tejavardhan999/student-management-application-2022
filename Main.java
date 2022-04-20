694c9161bbe310718bf150481bea8a0844555e6c
D.Tejavardhan <tejavardhan.rise@gmail.com>
2022-04-20 16:29:07 +0530

    Taking Argument means it print that argument.Not means it shows No argument is given

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args){
+    if(args.length>0){
+        for(int i=0;i<args.length;i++){
+               System.out.println(args[i]);
+        }
+    }
+    else{
+        System.out.println("No argument given.");
+    }
+    }
+
+}
