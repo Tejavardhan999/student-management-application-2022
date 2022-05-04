4d17202710feeddcc9792f90c85c18c40fa375ec
P. Vasavi <103931321+Vasavi711@users.noreply.github.com>
2022-05-04 16:26:55 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main
 {
-    public static void main(String[] args){
+    public static void main(String[] args) 
+    {
     if(args.length>0){
                System.out.println(args[0]);
-        }
+                     }
     else{
         System.out.println("No argument given.");
+        }
     }
-}
 }
